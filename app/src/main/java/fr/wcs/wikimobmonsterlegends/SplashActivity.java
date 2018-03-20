68874500f508cd3e874477ea4015ee2293114309
bastienwcs <bastien@wildcodeschool.fr>
2018-03-20 12:28:50 +0100

    fixes

--- app/src/main/java/fr/wcs/wikimobmonsterlegends/SplashActivity.java
+++ app/src/main/java/fr/wcs/wikimobmonsterlegends/SplashActivity.java
@@ -17,11 +17,8 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 Intent i = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(i);
-                finish();
-
             }
 
         }, SPLASH_TIME_OUT);
